4140f1cf105fd8c0433af3631cb3cb2097121ff4
afrodetka <Pro100biv@mail.ru>
2024-06-04 17:37:41 +0400

    Реализация структуры и методов

---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+
+template<typename Vertex, typename Distance = double>
+class Graph {
+public:
+    struct Edge {
+        Vertex from;
+        Vertex to;
+        Distance distance;
+
+        bool operator==(const Edge& other) const {
+            return from == other.from && to == other.to && distance == other.distance;
+        }
+    };
+    //проверка-добавление-удаление вершин
+    bool has_vertex(const Vertex& v) const;
+    void add_vertex(const Vertex& v);
+    bool remove_vertex(const Vertex& v);
+    std::vector<Vertex> vertices() const;
+
+
+    //проверка-добавление-удаление ребер
+    void add_edge(const Vertex& from, const Vertex& to,
+        const Distance& d);
+    bool remove_edge(const Vertex& from, const Vertex& to);
+    bool remove_edge(const Edge& e); //c учетом расстояния
+    bool has_edge(const Vertex& from, const Vertex& to) const;
+    bool has_edge(const Edge& e) const; //c учетом расстояния в Edge
+
+    //получение всех ребер, выходящих из вершины
+    std::vector<Edge> edges(const Vertex& vertex);
+
+    size_t order() const; //порядок 
+    size_t degree(const Vertex& v) const; //степень вершины
+
+
+    //поиск кратчайшего пути
+    std::vector<Edge> shortest_path(const Vertex& from,
+        const Vertex& to) const;
+    //обход
+    std::vector<Vertex>  walk(const Vertex& start_vertex)const;
+
+
+private:
+    unordered_map<Vertex, std::unordered_map<Vertex, Distance>> adjacency_list;
+};
+
